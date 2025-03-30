4622d0a8711a247671ac31a255ae7be0464f6f61
Lagooneng <hyunwoo122490@gmail.com>
2025-03-30 19:39:26 +0900

    가드 만드는 중

--- Source/DragDown/Character/DDCharacterPlayer.cpp
+++ Source/DragDown/Character/DDCharacterPlayer.cpp
@@ -70,6 +70,12 @@ ADDCharacterPlayer::ADDCharacterPlayer()
 		PushingAction = InputActionPushingRef.Object;
 	}
 
+	static ConstructorHelpers::FObjectFinder<UInputAction> InputActionGaurdRef(TEXT("/Script/EnhancedInput.InputAction'/Game/Input/Actions/IA_Gaurd.IA_Gaurd'")); 
+	if (nullptr != InputActionGaurdRef.Object)
+	{
+		GaurdAction = InputActionGaurdRef.Object;
+	}
+
 	static ConstructorHelpers::FObjectFinder<USkeletalMesh> CharacterMeshRef(TEXT("/Script/Engine.SkeletalMesh'/Game/Characters/Mannequins/Meshes/SKM_Quinn.SKM_Quinn'"));
 	if (CharacterMeshRef.Object)
 	{
@@ -108,7 +114,7 @@ void ADDCharacterPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputC
 	EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping); 
 	EnhancedInputComponent->BindAction(ShoulderMoveAction, ETriggerEvent::Triggered, this, &ADDCharacterPlayer::ShoulderMove);
 	EnhancedInputComponent->BindAction(ShoulderLookAction, ETriggerEvent::Triggered, this, &ADDCharacterPlayer::ShoulderLook); 
-
+	
 	SetupGASInputComponent();
 }
 
@@ -159,8 +165,10 @@ void ADDCharacterPlayer::SetupGASInputComponent()
 		UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
 
 		//추가 gas 액션
-		EnhancedInputComponent->BindAction(PushingAction, ETriggerEvent::Triggered, this, &ADDCharacterPlayer::GASInputPressed, 1);
+		EnhancedInputComponent->BindAction(PushingAction, ETriggerEvent::Started, this, &ADDCharacterPlayer::GASInputPressed, 1);
 		EnhancedInputComponent->BindAction(PushingAction, ETriggerEvent::Completed, this, &ADDCharacterPlayer::GASInputReleased, 1);
+		EnhancedInputComponent->BindAction(GaurdAction, ETriggerEvent::Started, this, &ADDCharacterPlayer::GASInputPressed, 2);
+		EnhancedInputComponent->BindAction(GaurdAction, ETriggerEvent::Canceled, this, &ADDCharacterPlayer::GASInputReleased, 2);
 
 		UE_LOG(LogDD, Log, TEXT("SetupGASInputComponent Succeed"));
 	}
@@ -202,8 +210,8 @@ void ADDCharacterPlayer::HandleGASInputPressed(int32 InputId)
 	FGameplayAbilitySpec* Spec = ASC->FindAbilitySpecFromInputID(InputId);
 	if (Spec)
 	{
-		if (Spec->InputPressed) return;
-		Spec->InputPressed = true;
+		/*if (Spec->InputPressed) return;
+		Spec->InputPressed = true;*/
 		if (Spec->IsActive())
 		{
 			ASC->AbilitySpecInputPressed(*Spec);
@@ -235,7 +243,7 @@ void ADDCharacterPlayer::HandleGASInputReleased(int32 InputId)
 	FGameplayAbilitySpec* Spec = ASC->FindAbilitySpecFromInputID(InputId);
 	if (Spec)
 	{
-		Spec->InputPressed = false;
+		//Spec->InputPressed = false;
 		if (Spec->IsActive())
 		{
 			ASC->AbilitySpecInputReleased(*Spec);
